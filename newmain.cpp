46f2a5b6a0b2a57b04c2ea0d1b5ae529801c952a
IritaSee <iga.narendra@gmail.com>
2024-06-10 05:01:39 +0900

    change testing data to bepridil

--- newmain.cpp
+++ newmain.cpp
@@ -164,7 +164,7 @@ int main(int argc, char **argv)
   // drug initialisation
   drug_t IC50;
   bool is_cai_scaling = true;
-  double conc = 38.0*0;
+  double conc = 33.0;
   double bcl = 1000.;
 
   // cell object pointer
